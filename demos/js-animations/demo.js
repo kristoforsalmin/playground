060deb3a52c3269b35a2239438f3bcf73be11890
Kristofor Salmin <4795426+kristoforsalmin@users.noreply.github.com>
2017-06-14 13:03:23 +0300

    Use DOMContentLoaded event

--- demos/js-animations/demo.js
+++ demos/js-animations/demo.js
@@ -24,15 +24,17 @@ function getKeyframes() {
   return [center, ...points, center];
 }
 
-const box = document.querySelector('.js-box');
-const boxImage = box.querySelector('.js-boxImage');
-const animation = boxImage.animate(getKeyframes(), {
-  duration: 1000,
-  easing: 'cubic-bezier(0.42, 0, 0.58, 1)',
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const box = document.querySelector('.js-box');
+  const boxImage = box.querySelector('.js-boxImage');
+  const animation = boxImage.animate(getKeyframes(), {
+    duration: 1000,
+    easing: 'cubic-bezier(0.42, 0, 0.58, 1)',
+  });
 
-animation.pause();
+  animation.pause();
 
-box.addEventListener('mouseenter', () => {
-  animation.play();
+  box.addEventListener('mouseenter', () => {
+    animation.play();
+  });
 });
